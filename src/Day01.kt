741df3fe84dfb2ea2093746839f512e80725ed1a
Thiyagu GK <thiyagu103@gmail.com>
2022-12-01 12:19:59 +0530

    day1 solution

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,17 +1,26 @@
 fun main() {
+    fun calculateCalories(input: List<String>) : List<Int> {
+        val calories = mutableListOf<Int>()
+        var accumulator = 0
+        input.forEach {
+            if (it.isBlank()) {
+                calories.add(accumulator)
+                accumulator = 0
+            }else {
+                accumulator += it.toInt()
+            }
+        }
+        return calories
+    }
+
     fun part1(input: List<String>): Int {
-        return input.size
+        return calculateCalories(input).maxOrNull()!!
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        return calculateCalories(input).reversed().take(3).sum()
     }
 
-    // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
-
-    val input = readInput("Day01")
-    println(part1(input))
-    println(part2(input))
+    println(part1(readInput("input/day01a")))
+    println(part2(readInput("input/day01b")))
 }
